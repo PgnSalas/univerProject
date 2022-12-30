03ecea343671c82b9357d78d35ac77e5923ecdcd
Gleb <gjivora@gmail.com>
2022-12-30 22:19:43 +0200

    The project 'ToDo' was done

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ const form = document.querySelector('#form'),
       taskList = document.querySelector('#tasksList'),
       emptyList = document.querySelector('#emptyList');
 
+let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach(task => renderTask(task));
+}
+
+checkEmptyList();
+
 taskList.addEventListener('click', doneTask);
 
 form.addEventListener('submit', addTask);
@@ -12,20 +21,95 @@ taskList.addEventListener('click', deleteTask);
 
 taskList.addEventListener('click', doneTask);
 
+
 function doneTask(e) {
-    if (e.target.dataset.action === 'done') {
+    if (e.target.dataset.action !== 'done') return;
+
         const parentNode = e.target.closest('.list-group-item');
+        
+     
+        const id = Number(parentNode.id);
+
+        const task = tasks.find((task) => task.id === id)
+
+        task.done = !task.done;
+
+        saveToLocalStorage();
+
         const taskTitle = parentNode.querySelector('.task-title');
-        taskTitle.classList.toggle('task-title--done');
-    };
+        taskTitle.classList.toggle('task-title--done')   
 }
 
 function addTask(e) {
     e.preventDefault();
 
     const taskText = taskInput.value;
-    const taskHtml = `<li class="list-group-item d-flex justify-content-between task-item">
-                        <span class="task-title">${taskText}</span>
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false
+    };
+
+    tasks.push(newTask);
+
+    saveToLocalStorage();
+
+    // css class
+    renderTask(newTask);
+
+    // clean field in input
+    taskInput.value = '';
+    taskInput.focus();
+
+    checkEmptyList();
+}
+
+function deleteTask(e) {
+    if (e.target.dataset.action !== 'delete') return;
+
+    const parentNode = e.target.closest('.list-group-item');
+
+    // find out ID
+    const id = parentNode.id;
+    
+    const index = tasks.findIndex(task => task.id === id);
+
+   tasks.splice(index, 1);
+
+   saveToLocalStorage();
+
+    parentNode.remove();
+
+    checkEmptyList();
+} 
+
+function checkEmptyList() {
+    if (tasks.length == 0) {
+        const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+        <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+        <div class="empty-list__title">Список дел пуст</div>
+        </li>`;
+
+
+        taskList.insertAdjacentHTML('afterbegin', emptyListHTML);
+    } 
+
+    if (tasks.length > 0) {
+        const emptyListEl = document.querySelector('#emptyList');
+        emptyListEl ? emptyListEl.remove() : null;
+    }
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTask(task) {
+    const cssCLass = task.done ? 'task-title task-title--done' : 'task-title';
+
+    const taskHtml = `<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+                        <span class="${cssCLass}">${task.text}</span>
                         <div class="task-item__buttons">
                             <button type="button" data-action="done" class="btn-action">
                                 <img src="./img/tick.svg" alt="Done" width="18" height="18">
@@ -38,28 +122,4 @@ function addTask(e) {
 
     // add on the page
     taskList.insertAdjacentHTML("beforeend", taskHtml);
-
-    // clean field in input
-    taskInput.value = '';
-    taskInput.focus();
-
-    if (taskList.children.length > 1) {
-        emptyList.classList.add('none');
-    } 
 }
-
-function deleteTask(e) {
-    if (e.target.dataset.action !== 'delete') return;
-
-    const parentNode = e.target.closest('.list-group-item');
-    parentNode.remove();
-
-    if (taskList.children.length === 1) {
-        emptyList.classList.remove('none');
-    } 
-} 
-
-function saveHTMLtoLC() {
-    localStorage.setItem('tasksHTML', taskList.innerHTML);
-}
-
